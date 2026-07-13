901548827004ceb2f718df9166c1e9a31073a221
Shivam Mandal <mandalshiva444@gmail.com>
2026-07-13 18:01:37 +0545

    Khalti Payment tested

--- lib/khalti_payment_screen.dart
+++ lib/khalti_payment_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:khalti_checkout_flutter/khalti_checkout_flutter.dart';
+import 'package:khalti_payment_app/utils/khalti_services.dart';
 
 class KhaltiPaymentScreen extends StatefulWidget {
   const KhaltiPaymentScreen({super.key});
@@ -9,8 +12,63 @@ class KhaltiPaymentScreen extends StatefulWidget {
 }
 
 class _KhaltiPaymentScreenState extends State<KhaltiPaymentScreen> {
-  late final Future<Khalti?> khalti;
+  // late final Future<Khalti?> khalti;
+  Khalti? _khalti;
   bool isLoading = false;
+  PaymentResult? paymentResult;
+
+  Future<void> payWithKhaltiCheckOut(int amount) async {
+    try {
+      setState(() {
+        isLoading = true;
+      });
+
+      // ----------------------- Step First----------------------------------------
+      final pidxNumber = await KhaltiServices.generatePidx(amount);
+      print('Pidx Number is: ${pidxNumber}');
+
+      // --------------------------Step Second-------------------------------
+      final config = KhaltiPayConfig(
+        paymentUrl:
+            'https://test-pay.khalti.com/?pidx=$pidxNumber&return_url=https%3A%2F%2Fdocs.khalti.com%2Fkhalti-epayment&mode=wallet',
+        publicKey: '3a895daf55d54e6a8937dbec60fc0a69',
+        pidx: pidxNumber!,
+        environment: Environment.test,
+      );
+      // --------------------------Step Third-------------------------------
+      _khalti = await Khalti.init(
+        enableDebugging: true,
+        payConfig: config,
+        onPaymentResult: (paymentResult, khalti) {
+          log(paymentResult.toString());
+          setState(() {
+            this.paymentResult = paymentResult;
+          });
+          khalti.close(context);
+        },
+        onMessage:
+            (
+              khalti, {
+              description,
+              statusCode,
+              event,
+              needsPaymentConfirmation,
+            }) async {
+              log(
+                'Description: $description, Status Code: $statusCode, Event: $event, NeedsPaymentConfirmation: $needsPaymentConfirmation',
+              );
+              khalti.close(context);
+            },
+        onReturn: () => log('Successfully redirected to return_url.'),
+      );
+
+      // --------------------------Step Third-------------------------------
+    } catch (e) {
+      log('Inilization Error: ${e.toString()}');
+    } finally {
+      isLoading = false;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,26 +81,23 @@ class _KhaltiPaymentScreenState extends State<KhaltiPaymentScreen> {
       ),
 
       body: Center(
-        child: Column(
-          mainAxisAlignment: .center,
-          spacing: 10,
-          children: [
-            Text('Khalti Payment'),
-            ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: WidgetStatePropertyAll(Colors.green[100]),
-                padding: .all(
-                  EdgeInsets.symmetric(horizontal: 30, vertical: 8),
+        child: isLoading
+            ? const CircularProgressIndicator()
+            : ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(Colors.green[100]),
+                  padding: .all(
+                    EdgeInsets.symmetric(horizontal: 30, vertical: 8),
+                  ),
+                ),
+                onPressed: () {
+                  payWithKhaltiCheckOut(100);
+                },
+                child: Text(
+                  'Pay 💳',
+                  style: TextStyle(fontSize: 24, fontWeight: .bold),
                 ),
               ),
-              onPressed: () {},
-              child: Text(
-                'Pay 💳',
-                style: TextStyle(fontSize: 24, fontWeight: .bold),
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
